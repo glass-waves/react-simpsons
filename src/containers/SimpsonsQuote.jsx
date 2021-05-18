4956f06b00bc2f97925c94294275c4c02a5779aa
Dylan White <dylan.whitej@gmail.com>
2021-05-18 11:16:31 -0700

    app working

--- src/containers/SimpsonsQuote.jsx
+++ src/containers/SimpsonsQuote.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import fetchQuote from '../services/fetchQuote';
+import {fetchQuote} from '../services/fetchQuote';
+import Load from '../components/quote/Load'
+import Quote from '../components/quote/Quote'
 
 const SimpsonsQuote = () => {
     const [quote, setQuote] = useState({});
 
     const onButtonPress = async () => {
         const quote = await fetchQuote();
+        setQuote(quote)
     };
     return (
         <div>
             <Load onPress={onButtonPress}/>
-            {!!quote.length && <Quote quote={quote}/>}
+            {!!quote.length && <Quote quote={quote[0]}/>}
         </div>
     );
 };
--- src/services/fetchQuote.js
+++ src/services/fetchQuote.js
@@ -1,4 +1,4 @@
-export default fetchQuote = async() => {
+export const fetchQuote = async() => {
     const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
     const quote = await res.json();
     return quote;  
